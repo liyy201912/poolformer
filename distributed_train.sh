4e7406c8718f173def88cbba6949898f5b37692a
Yanyu <liyy201912@gmail.com>
2022-05-05 21:17:30 -0400

    Update distributed_train.sh

--- distributed_train.sh
+++ distributed_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NUM_PROC=$1
-shift
-python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC train.py "$@"
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+python -m torch.distributed.launch --nproc_per_node=8 train.py /home/ImageNet --model poolformer_s24 -b 128 --lr 2e-3 --drop-path 0.1 --apex-amp
 
